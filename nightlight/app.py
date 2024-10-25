ebb213782035b1aad822cb8422bebd8f30995cd9
synmutanX <maulanasofyan0712@gmail.com>
2024-10-25 23:54:05 +0700

    feat: add light sensor

--- nightlight/app.py
+++ nightlight/app.py
@@ -1,5 +1,15 @@
+import time
+from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
+
 from counterfit_connection import CounterFitConnection
 
 CounterFitConnection.init('127.0.0.1', 5000)
 
 print("Hello IoT World!")
+
+light_sensor = GroveLightSensor(0)
+
+while True:
+    light = light_sensor.light
+    print('Light level:', light)
+    time.sleep(1)
