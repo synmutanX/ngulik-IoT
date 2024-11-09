0dde7f15db68f070407b21d8a7a5cc48d53afff6
synmutanX <maulanasofyan0712@gmail.com>
2024-11-09 13:29:39 +0700

    Send commands to the MQTT broker

--- nighlight-server/app.py
+++ nighlight-server/app.py
@@ -6,6 +6,7 @@ import paho.mqtt.client as mqtt
 id = '67d75ceb-5e94-4531-8472-c05f7864e462'
 
 client_telemetry_topic = id + '/telemetry'
+server_command_topic = id + '/commands'
 client_name = id + 'nightlight-server'
 
 protocol_version = mqtt.MQTTv5
@@ -18,6 +19,11 @@ mqtt_client.loop_start()
 def handle_telemetry(client, userdata, message):
     payload = json.loads(message.payload.decode())
     print("Message received: ", payload)
+
+    command = { 'led_on' : payload['light'] < 300}
+    print("Sending message:", command)
+
+    client.publish(server_command_topic, json.dumps(command))
 
 mqtt_client.subscribe(client_telemetry_topic)
 mqtt_client.on_message = handle_telemetry
