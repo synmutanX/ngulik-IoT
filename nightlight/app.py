c832005e3dfc1c6d5a6281e7f0382c8c94575472
synmutanX <maulanasofyan0712@gmail.com>
2024-11-16 17:27:03 +0700

    feat: subscribe to the commands sent from the MQTT broker and respond to them

--- nightlight/app.py
+++ nightlight/app.py
@@ -19,6 +19,7 @@ id = '67d75ceb-5e94-4531-8472-c05f7864e462'
 client_name = id + 'nightlight_client'
 
 client_telemetry_topic = id + '/telemetry'
+server_command_topic = id + '/commands'
 
 # Use the appropriate protocol version (MQTTv5 or MQTTv311)
 protocol_version = mqtt.MQTTv5  # Use mqtt.MQTTv311 if you're working with MQTT 3.1.1
@@ -26,11 +27,42 @@ protocol_version = mqtt.MQTTv5  # Use mqtt.MQTTv311 if you're working with MQTT 
 # Pass the client_id explicitly to avoid the multiple values error
 mqtt_client = mqtt.Client(client_id=client_name, protocol=protocol_version)
 
-# Connect to the MQTT broke
+# Connect to the MQTT broker
 mqtt_client.connect('test.mosquitto.org')
 
 # Start the MQTT client loop in the background
 mqtt_client.loop_start()
+
+def handle_command(client, userdata, message):
+    """
+    Handle MQTT command messages from the CounterFit server.
+
+    This is a callback function that is called whenever an MQTT message is received from the
+    CounterFit server with a topic matching the server_command_topic. The message payload is
+    expected to contain a JSON object with a single key-value pair. The key is named
+    'led_on' and the value is a boolean (True or False). If the value is True, the LED
+    is turned on; otherwise, the LED is turned off.
+
+    Parameters:
+        client: The MQTT client that received the message.
+        userdata: User-defined data associated with the MQTT client.
+        message: The MQTT message that was received.
+
+    Returns:
+        None
+    """
+    # Decodes the message payload as JSON
+    payload = json.loads(message.payload.decode())
+    print("message received: ", payload)
+
+    # Turns the LED on or off based on the value of the 'led_on' key in the JSON payload (True for on, False for off)
+    if payload['led_on']:
+        led.on()
+    else:
+        led.off()
+
+mqtt_client.subscribe(server_command_topic)
+mqtt_client.on_message = handle_command
 
 print("MQTT Connected!")
 
@@ -49,7 +81,8 @@ while True:
     light = light_sensor.light
     telemetry = json.dumps({'light': light})
 
-    print("Sending telemetry ", telemetry)
+    # print("Sending telemetry ", telemetry)
+    print('Light level:', light)
 
     mqtt_client.publish(client_telemetry_topic, telemetry)
 
